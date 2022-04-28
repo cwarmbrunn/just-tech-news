fe23d13e8638b82d1cbcc960d230158ab7807ac6
cwarmbrunn <cwarmbrunn97@gmail.com>
2022-04-28 18:57:52 -0400

    added MVP

---
+++ routes/api/comment-routes.js
@@ -0,0 +1,65 @@
+const router = require("express").Router();
+const { Comment, Post, User } = require("../../models");
+const { sequelize } = require("../../models/Post");
+
+router.get("/", (req, res) => {
+  Comment.findAll({
+    order: [["created_at", "DESC"]],
+    attributes: [
+      "id",
+      "post_url",
+      "title",
+      "created_at",
+      [
+        sequelize.literal(
+          `(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)`
+        ),
+        "vote_count",
+      ],
+    ],
+    include: [
+      // Include the Comment model here:
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      { model: User, attributes: ["username"] },
+    ],
+  });
+});
+
+router.post("/", (req, res) => {
+  Comment.create({
+    comment_text: req.body.comment_text,
+    user_id: req.body.user_id,
+    post_id: req.body.post_id,
+  })
+    .then((dbCommentData) => res.json(dbCommentData))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  Comment.destroy({
+    where: { id: req.params.id },
+  })
+    .then((dbCommentData) => {
+      if (!dbCommentData) {
+        res.status(404).json({ message: "No comment found!" });
+        return;
+      }
+      res.json(dbCommentData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
